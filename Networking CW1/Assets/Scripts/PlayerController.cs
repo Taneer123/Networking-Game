980d7a084d97734c37190fedef008e970b28452e
Taneer123 <60103115+Taneer123@users.noreply.github.com>
2020-01-20 15:20:42 +0000

    added unity

---
+++ Networking CW1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    const int MOVEMENT_RATE = 30;
+    const int ROTATION_RATE = 150;
+
+
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * ROTATION_RATE;
+        var verticallInput = Input.GetAxis("Vertical") * Time.deltaTime * MOVEMENT_RATE;
+
+        transform.Rotate(0, horizontalInput, 0);
+        transform.Translate(0, 0, verticallInput);
+    }
+
+}
